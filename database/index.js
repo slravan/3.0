c45de25ed60245d3499e51cf18326ed06a727504
slravan <92201989+slravan@users.noreply.github.com>
2022-04-06 22:42:40 -0700

    Add files via upload
    /* Copyright © 2022 Thisan04.
    
    Licensed under the  GPL-3.0 License;
    you may not use this file except in compliance with the License.
    
    ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
    ████▌▄▌▄▐▐▌█████
    ████▌▄▌▄▐▐▌▀████
    ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
    
    */

---
+++ database/index.js
@@ -0,0 +1,20 @@
+/* Copyright © 2022 Thisan04.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+████▌▄▌▄▐▐▌█████
+████▌▄▌▄▐▐▌▀████
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
